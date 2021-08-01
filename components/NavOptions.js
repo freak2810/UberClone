3bdce550269ac6950d576c5a8c718151f74ff93c
freak2810 <adityamanikanthrao@gmail.com>
2021-08-01 17:14:11 +0530

    Started home screen

---
+++ components/NavOptions.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, StyleSheet, Text, FlatList, TouchableOpacity, Image} from "react-native";
+import tw from "tailwind-react-native-classnames";
+import {Icon} from "react-native-elements";
+
+const data = [
+    {
+        id: '123',
+        title: 'Get a ride',
+        image: 'https://links.papareact.com/3pn',
+        screen: 'MapScreen'
+    },
+    {
+        id: '456',
+        title: 'Order Food',
+        image: 'https://links.papareact.com/28w',
+        screen: 'EatScreen' // Future
+    }
+]
+
+export default function NavOptions() {
+    return <FlatList
+        data={data}
+        horizontal
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+            <View>
+                <Image source={{uri: item.image}} style={{width: 120, height: 120, resizeMode: 'contain'}}/>
+            </View>
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} name={'arrowright'} color={'white'} type={'antdesign'}/>
+        </TouchableOpacity>}/>
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/MapScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View, StyleSheet} from "react-native";
+
+export default function MapScreen() {
+    return <View>
+        <Text>Here is the map screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({});
