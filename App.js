a325bff81deb245bc671df612d454fac9b84f8b0
freak2810 <adityamanikanthrao@gmail.com>
2021-08-01 18:12:29 +0530

    Added react navigation

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {store} from "./store";
 import HomeScreen from "./screens/HomeScreen";
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from '@react-navigation/stack';
+import MapScreen from "./screens/MapScreen";
 
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <Provider store={store}>
-            <SafeAreaProvider>
-                <HomeScreen/>
-            </SafeAreaProvider>
+            <NavigationContainer>
+                <SafeAreaProvider>
+                    <Stack.Navigator>
+                        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
+                        <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown: false}}/>
+                    </Stack.Navigator>
+                </SafeAreaProvider>
+            </NavigationContainer>
         </Provider>
     );
 }
